ffe8ea6d59523025d16216ea1d99616df9e0b0ce
aj <andrewjpack@gmail.com>
2019-03-08 14:43:51 +0000

    fixed polybar duplication

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
+#polybar top &
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
 
 echo "Bars launched..."
