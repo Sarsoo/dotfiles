d59920ffaba852917e84f438b6b55cb77aa9e95f
Andy Pack <andy@sarsoo.xyz>
2026-06-25 19:11:04 +0100

    function-ing init.sh, adding swah and rofi config

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 # /usr/bin/bash
-set -x
+# set -x
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -14,47 +14,61 @@ case "${unameOut}" in
 esac
 echo "Init-ing [${machine}]"
 
+function link_file(){
+  local source="${SCRIPT_DIR}/${1}"
+  local destination="${HOME}/${2}"
+
+  mkdir -p "$(dirname "${destination}")"
+
+  if [ -d "${destination}" ]; then
+
+    echo "removing dir (${destination})"
+    rm -rf "${destination}"
+
+  elif [ -f "${destination}" ]; then
+
+    echo "removing file (${destination})"
+    rm "${destination}"
+
+  elif [ -L "${destination}" ]; then
+
+    echo "removing link (${destination})"
+    rm "${destination}"
+
+  fi
+
+  echo "linking (${source}) -> (${destination})"
+  ln -s "${source}" "${destination}"
+}
+
+mkdir -p "${HOME}/.config"
+mkdir -p "${HOME}/.config/systemd/user"
+
 ############
 # BashRC
 ############
-
-rm $HOME/.bashrc
-ln -s $SCRIPT_DIR/.bashrc $HOME/.bashrc
+link_file ".bashrc" ".bashrc"
 
 ############
 # ZshRC
 ############
-
-rm $HOME/.zshrc
-ln -s $SCRIPT_DIR/.zshrc $HOME/.zshrc
+link_file ".zshrc" ".zshrc"
 
 ############
 # SaRC
 ############
-
-rm $HOME/.sarc
-ln -s $SCRIPT_DIR/.sarc $HOME/.sarc
+link_file ".sarc" ".sarc"
 
 ############
 # profile
 ############
-
-rm $HOME/.profile
-ln -s $SCRIPT_DIR/.profile $HOME/.profile
+link_file ".profile" ".profile"
 
 ############
 # zprofile
 ############
-
-rm $HOME/.zprofile
-ln -s $SCRIPT_DIR/.zprofile $HOME/.zprofile
-
-############
-# zprofile
-############
-
-rm $HOME/.zshenv
-ln -s $SCRIPT_DIR/.zshenv $HOME/.zshenv
+link_file ".zprofile" ".zprofile"
+link_file ".zshenv" ".zshenv"
 
 ############
 # SSH
@@ -76,80 +90,44 @@ ln -s $SCRIPT_DIR/.zshenv $HOME/.zshenv
 ############
 # Starship
 ############
-
-if [ -e "$HOME/.config/starship.toml" ]; then
-    rm $HOME/.config/starship.toml
-fi
-ln -s $SCRIPT_DIR/starship/starship.toml $HOME/.config/starship.toml
+link_file "starship/starship.toml" ".config/starship.toml"
 
 ############
 #   VIM
 ############
-
-if [ -e "$HOME/.vimrc" ]; then
-    rm $HOME/.vimrc
-fi
-ln -s $SCRIPT_DIR/vim/.vimrc $HOME/.vimrc
+link_file "vim/.vimrc" ".vimrc"
+link_file "vim/plugins.vim" ".vim/plugins.vim"
 
 if [ ! -d "$HOME/.vim" ]; then
     # Directory does not exist, so create it
     mkdir "$HOME/.vim"
 fi
-if [ -e "$HOME/.vim/plugins.vim" ]; then
-    rm $HOME/.vim/plugins.vim
-fi
 if [ ! -d "$HOME/.vim/bundle" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
-ln -s $SCRIPT_DIR/vim/plugins.vim $HOME/.vim/plugins.vim
 vim +PluginInstall +qall
 
 ############
 #  RANGER
 ############
-
-if [ -d "$HOME/.config/ranger" ]; then
-    rm -rf $HOME/.config/ranger
-fi
-ln -s $SCRIPT_DIR/ranger $HOME/.config/ranger
+link_file "ranger" ".config/ranger"
 
 ############
 # ALACRITTY
 ############
-
-if [ -d "$HOME/.config/alacritty" ]; then
-    rm -rf $HOME/.config/alacritty
-fi
-ln -s $SCRIPT_DIR/alacritty $HOME/.config/alacritty
+link_file "alacritty" ".config/alacritty"
 
 ############
 #  HALLOY
 ############
-
-if [ -d "$HOME/.var/app/org.squidowl.halloy/config/halloy" ]; then
-    rm -rf $HOME/.var/app/org.squidowl.halloy/config/halloy
-fi
-ln -s $SCRIPT_DIR/halloy $HOME/.var/app/org.squidowl.halloy/config/halloy
+link_file "halloy" ".var/app/org.squidowl.halloy/config/halloy"
 
 ############
 #  ZED
 ############
-
-if [ ! -d "$HOME/.config/zed" ]; then
-    mkdir $HOME/.config/zed
-fi
-if [ -e "$HOME/.config/zed/settings.json" ]; then
-    rm $HOME/.config/zed/settings.json
-fi
-if [ -e "$HOME/.config/zed/keymap.json" ]; then
-    rm $HOME/.config/zed/keymap.json
-fi
-if [ -e "$HOME/.config/zed/tasks.json" ]; then
-    rm $HOME/.config/zed/tasks.json
-fi
-ln -s $SCRIPT_DIR/zed/settings.json $HOME/.config/zed/settings.json
-ln -s $SCRIPT_DIR/zed/keymap.json $HOME/.config/zed/keymap.json
-ln -s $SCRIPT_DIR/zed/tasks.json $HOME/.config/zed/tasks.json
+link_file "zed/settings.json" ".config/zed/settings.json"
+link_file "zed/keymap.json" ".config/zed/keymap.json"
+link_file "zed/tasks.json" ".config/zed/tasks.json"
 
 ############
 #  VSCODE
@@ -158,17 +136,11 @@ ln -s $SCRIPT_DIR/zed/tasks.json $HOME/.config/zed/tasks.json
 case $machine in
 
     Mac)
-        if [ -e "$HOME/Library/Application Support/Code/User/settings.json" ]; then
-            rm "$HOME/Library/Application Support/Code/User/settings.json"
-        fi
-        ln -s $SCRIPT_DIR/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+        link_file "vscode/settings.json" "Library/Application Support/Code/User/settings.json"
     ;;
 
     Linux)
-        if [ -e "$HOME/.config/Code/User/settings.json" ]; then
-            rm $HOME/.config/Code/User/settings.json
-        fi
-        ln -s $SCRIPT_DIR/vscode/settings.json "$HOME/.config/Code/User/settings.json"
+        link_file "vscode/settings.json" ".config/Code/User/settings.json"
     ;;
 
 esac
@@ -176,32 +148,17 @@ esac
 ############
 #  HTOP
 ############
-
-if [ ! -d "$HOME/.config/htop" ]; then
-    mkdir $HOME/.config/htop
-fi
-if [ -e "$HOME/.config/htop/htoprc" ]; then
-    rm $HOME/.config/htop/htoprc
-fi
-ln -s $SCRIPT_DIR/htop/htoprc $HOME/.config/htop/htoprc
+link_file "htop/htoprc" ".config/htop/htoprc"
 
 ############
 #  LINEAR MOUSE
 ############
-
-if [ -d "$HOME/.config/linearmouse" ]; then
-    rm -rf $HOME/.config/linearmouse
-fi
-ln -s $SCRIPT_DIR/linearmouse $HOME/.config/linearmouse
+link_file "linearmouse" ".config/linearmouse"
 
 ############
 #  npm
 ############
-
-if [ -e "$HOME/.npmrc" ]; then
-    rm $HOME/.npmrc
-fi
-ln -s $SCRIPT_DIR/npm/npmrc $HOME/.npmrc
+link_file "npm/npmrc" ".npmrc"
 
 ############
 #   K9S
@@ -210,37 +167,15 @@ ln -s $SCRIPT_DIR/npm/npmrc $HOME/.npmrc
 case $machine in
 
     Mac)
-        if [ -e "$HOME/Library/Application Support/k9s/aliases.yaml" ]; then
-            rm "$HOME/Library/Application Support/k9s/aliases.yaml"
-        fi
-        ln -s $SCRIPT_DIR/k9s/aliases.yaml "$HOME/Library/Application Support/k9s/aliases.yaml"
-
-        if [ -e "$HOME/Library/Application Support/k9s/config.yaml" ]; then
-            rm "$HOME/Library/Application Support/k9s/config.yaml"
-        fi
-        ln -s $SCRIPT_DIR/k9s/config.yaml "$HOME/Library/Application Support/k9s/config.yaml"
-
-        if [ -e "$HOME/Library/Application Support/k9s/views.yaml" ]; then
-            rm "$HOME/Library/Application Support/k9s/views.yaml"
-        fi
-        ln -s $SCRIPT_DIR/k9s/config.yaml "$HOME/Library/Application Support/k9s/views.yaml"
+        link_file "k9s/aliases.yaml" "Library/Application Support/k9s/aliases.yaml"
+        link_file "k9s/config.yaml" "Library/Application Support/k9s/config.yaml"
+        link_file "k9s/views.yaml" "Library/Application Support/k9s/views.yaml"
     ;;
 
     Linux)
-        if [ -e "$HOME/.config/k9s/aliases.yaml" ]; then
-            rm "$HOME/.config/k9s/aliases.yaml"
-        fi
-        ln -s $SCRIPT_DIR/k9s/aliases.yaml "$HOME/.config/k9s/aliases.yaml"
-
-        if [ -e "$HOME/.config/k9s/config.yaml" ]; then
-            rm "$HOME/.config/k9s/config.yaml"
-        fi
-        ln -s $SCRIPT_DIR/k9s/config.yaml "$HOME/.config/k9s/config.yaml"
-
-        if [ -e "$HOME/.config/k9s/views.yaml" ]; then
-            rm "$HOME/.config/k9s/views.yaml"
-        fi
-        ln -s $SCRIPT_DIR/k9s/config.yaml "$HOME/.config/k9s/views.yaml"
+        link_file "k9s/aliases.yaml" ".config/k9s/aliases.yaml"
+        link_file "k9s/config.yaml" ".config/k9s/config.yaml"
+        link_file "k9s/views.yaml" ".config/k9s/views.yaml"
     ;;
 
 esac
@@ -248,42 +183,30 @@ esac
 ############
 #  flux9s
 ############
-
-if [ -e "$HOME/.config/flux9s" ]; then
-    rm -rf $HOME/.config/flux9s
-fi
-ln -s $SCRIPT_DIR/flux9s $HOME/.config/flux9s
+link_file "flux9s" ".config/flux9s"
 
 ############
-#  flux9s
+# opencode
 ############
-
-if [ ! -d "$HOME/.config/opencode" ]; then
-    mkdir $HOME/.config/opencode
-fi
-if [ -e "$HOME/.config/opencode/opencode.jsonc" ]; then
-    rm -rf $HOME/.config/opencode/opencode.jsonc
-fi
-if [ -e "$HOME/.config/opencode/tui.jsonc" ]; then
-    rm -rf $HOME/.config/opencode/tui.jsonc
-fi
-ln -s $SCRIPT_DIR/opencode/opencode.jsonc $HOME/.config/opencode/opencode.jsonc
-ln -s $SCRIPT_DIR/opencode/tui.jsonc $HOME/.config/opencode/tui.jsonc
+link_file "opencode/opencode.jsonc" ".config/opencode/opencode.jsonc"
+link_file "opencode/tui.jsonc" ".config/opencode/tui.jsonc"
 
 ################
 #  aerospace
 ################
-
-if [ -e "$HOME/.aerospace.toml" ]; then
-    rm $HOME/.aerospace.toml
-fi
-ln -s $SCRIPT_DIR/aerospace/.aerospace.toml $HOME/.aerospace.toml
+link_file "aerospace/.aerospace.toml" ".aerospace.toml"
 
 ################
 #   tmux
 ################
+link_file "tmux/tmux.conf" ".tmux.conf"
 
-if [ -e "$HOME/.tmux.conf" ]; then
-    rm $HOME/.tmux.conf
-fi
-ln -s $SCRIPT_DIR/tmux/tmux.conf $HOME/.tmux.conf
+################
+#   sway
+################
+link_file "sway/config" ".config/sway/config"
+
+################
+#   rofi
+################
+link_file "rofi/config.rasi" ".config/rofi/config.rasi"
